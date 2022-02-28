88e224d342dfda5a74ea5f728f72aac2c6a6e12f
Bartekkom <bartek.kom524@gmail.com>
2022-02-28 21:31:24 +0100

    initializing repository

---
+++ MemoryLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class MemoryLogic
+{
+	private int x = 0;
+	public MemoryLogic(int x)
+	{
+		this.x = x;
+	}
+
+	public void message(string message)
+    {
+		Console.WriteLine(message);
+    }
+}
